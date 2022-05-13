522c4f37ef93ed08806ee3b7c54355f20fda6276
rewpt <zpermack@gmail.com>
2022-05-13 16:47:03 -0400

    use dispatch hook from redux to access state

--- src/hooks/useEngineRequest.js
+++ src/hooks/useEngineRequest.js
@@ -1,9 +1,12 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+import {useDispatch} from "react-redux";
+import { addInteraction } from '../redux/interactions';
 // import { Configuration, OpenAIApi } from "openai";
 
 export default function useEngineRequest(data, url, dependency) {
   const [response, setResponse] = useState([]);
+  const dispatch = useDispatch();
   useEffect(() => {
     
     // const getData = async () => {
@@ -28,9 +31,17 @@ export default function useEngineRequest(data, url, dependency) {
             Authorization: `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`,
           },
         })
-        .then((data) => setResponse(data.data.choices));
+        .then((res) => { 
+          const responseStr = res.data.choices
+          setResponse(responseStr);
+          dispatch(addInteraction({prompt: data.prompt, response: responseStr[0].text }))
+        });
+        
     };
-    sendAxReq(data);
+    console.log(data.prompt)
+    if(data.prompt.length > 1) {
+      sendAxReq(data);
+    }
   }, [dependency]);
   return response;
 }
