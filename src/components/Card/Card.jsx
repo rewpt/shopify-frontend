592db0282bfa2c36dcb7840a484cc6de46dc5cf1
rewpt <zpermack@gmail.com>
2022-05-13 16:48:54 -0400

    restructure components to be within folder sections

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Card = styled.div`
+  border: 1px solid black;
+  padding: 1rem;
+  background-color: #a1d0f4;
+`;
---
+++ src/components/Card/CardsContainer.jsx
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const CardsContainer = styled.div`
+  border: 2px solid black;
+  margin: 5vw;
+`;
