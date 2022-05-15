51ae58b070b7a0e784fd0b1e3a8d197153b19cb2
rewpt <zpermack@gmail.com>
2022-05-14 21:28:13 -0400

    change card styling size and border radius

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,4 +4,6 @@ export const Card = styled.div`
   border: 1px solid black;
   padding: 1rem;
   background-color: #a1d0f4;
+  border-radius: 15px;
+  max-width: 28%;
 `;
--- src/components/Card/CardsContainer.jsx
+++ src/components/Card/CardsContainer.jsx
@@ -3,4 +3,5 @@ import styled from "styled-components";
 export const CardsContainer = styled.div`
   border: 2px solid black;
   margin: 5vw;
+  padding: 2rem;
 `;
